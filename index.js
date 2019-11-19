42091a30902b04f93e79975fc901dcc649df1784
Grigorii Horos <horosgrisa@gmail.com>
2019-11-19 06:03:39 +0200

    Commit

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const parse5 = require('parse5');
-const { filterAst } = require('./lib/utils');
-const { textNode, body: bodyTag, i } = require('./lib/tags');
+const { html } = require('./lib/tags');
 
 const htmlToCli = (rawHTML) => {
   // @type Object
   const document = parse5.parse(rawHTML);
-  // console.log('___-----', document);
 
-
-  console.log(bodyTag(document, { pre: false, lineWidth: 80 }).value || '');
-
-  return '';
+  return `${(html(document, { pre: false, lineWidth: 80 }) || { value: '' }).value}\n`;
 };
 
 
