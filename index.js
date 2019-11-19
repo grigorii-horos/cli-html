a9142a5c26f8d8a2a359bf77e791543d9f6e0395
Grigorii Horos <horosgrisa@gmail.com>
2019-11-19 09:11:54 +0200

    Add tables

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const parse5 = require('parse5');
+const { filterAst } = require('./lib/utils');
 const { html } = require('./lib/tags');
 
 const htmlToCli = (rawHTML) => {
   // @type Object
   const document = parse5.parse(rawHTML);
+
+  // console.dir(filterAst(document.childNodes[0].childNodes[1]).childNodes, { depth: null });
 
   return `${(html(document, { pre: false, lineWidth: 80 }) || { value: '' }).value}\n`;
 };
