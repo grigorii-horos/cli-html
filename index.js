d1b7d41bf1ef0e05f872859f6f129c15facc817d
Grigorii Horos <horosgrisa@gmail.com>
2019-11-19 17:53:33 +0200

    Fix pre

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const parse5 = require('parse5');
+const htmlparser2Adapter = require('parse5-htmlparser2-tree-adapter');
+
 const { filterAst } = require('./lib/utils');
 const { html } = require('./lib/tags');
 
+
 const htmlToCli = (rawHTML) => {
   // @type Object
+
   const document = parse5.parse(rawHTML);
 
-  // console.dir(filterAst(document.childNodes[0].childNodes[1]).childNodes, { depth: null });
+  // console.dir(filterAst(document), { depth: null });
 
   return `${(html(document, { pre: false, lineWidth: 80 }) || { value: '' }).value}\n`;
 };
