7952c657c03a803de17db7d65f2d0290c2665076
Grigorii Horos <horosgrisa@gmail.com>
2019-11-22 13:13:16 +0200

    Fix concat

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const parse5 = require('parse5');
-const htmlparser2Adapter = require('parse5-htmlparser2-tree-adapter');
 
 const { filterAst } = require('./lib/utils');
 const { html } = require('./lib/tags');
