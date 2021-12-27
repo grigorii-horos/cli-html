451d3cc30de6f453b6863639ab301c6d1d761036
Grigorii Horos <horosgrisa@gmail.com>
2021-12-27 18:06:54 +0200

    Change parser

--- index.js
+++ index.js
@@ -1,38 +1,20 @@
 import parse5 from 'parse5';
 
-import { parse } from 'node-html-parser';
-import { filterAst, filterAst2 } from './lib/utils.js';
+import { filterAst } from './lib/utils.js';
 import { html } from './lib/tags/document.js';
 
 const htmlToCli = (rawHTML) => {
   // @type Object
 
-  // const document = parse5.parse(rawHTML);
-  const document2 = parse(rawHTML, {
-    blockTextElements: {
-      script: false,
-      noscript: false,
-      style: false,
-      // pre: true,
-      code: true,
-    },
-  });
+  const document = parse5.parse(rawHTML);
 
-  // console.dir(filterAst(document.childNodes[0].childNodes[1]), { depth: null });
-
-  // console.log("----");
-
-  // console.dir(filterAst2(document2), {
-  //   depth: null,
-  // });
+  // console.dir(
+  //   filterAst(document.childNodes[0].childNodes[1]),
+  //   { depth: null },
+  // );
 
   return `${
-    (
-      html(document2, {
-        pre: false,
-        lineWidth: +(process.env.CLI_HTML_LINE_WIDTH || '120'),
-      }) || { value: '' }
-    ).value
+    (html(document, { pre: false, lineWidth: 80 }) || { value: '' }).value
   }\n`;
 };
 
