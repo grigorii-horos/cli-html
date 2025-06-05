1e980037a2a090634d7cf81365ef6174fe3278e9
Grigorii Horos <horosgrisa@gmail.com>
2025-06-05 12:21:15 +0300

    Update

--- eslint.config.js
+++ eslint.config.js
@@ -12,16 +12,16 @@ const compat = new FlatCompat({
 export default [
   js.configs.recommended,
   jsonPlugin.configs.recommended,
-  unicornPlugin.configs['flat/recommended'],
+  unicornPlugin.configs["flat/recommended"],
   ...compat.extends(
-    'standard-jsdoc',
-    'airbnb-base'
+    "standard-jsdoc"
+    // 'airbnb-base'
   ),
   ...compat.plugins(
-    'no-loops',
-    'async-await',
-    'prefer-object-spread',
-    'simple-import-sort'
+    "no-loops",
+    "async-await",
+    "prefer-object-spread",
+    "simple-import-sort"
   ),
   ...compat.env({
     browser: true,
@@ -30,34 +30,34 @@ export default [
   {
     languageOptions: {
       ecmaVersion: 2020,
-      sourceType: 'module',
+      sourceType: "module",
       globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
       },
     },
     rules: {
-      'prefer-object-spread/prefer-object-spread': 2,
-      'no-restricted-globals': [2, ...restrictedGlobals],
-      'mocha/handle-done-callback': 0,
-      'mocha/no-global-tests': 0,
-      'mocha/valid-test-description': 0,
-      'import/extensions': 0,
-      'import/prefer-default-export': 0,
-      'import/no-cycle': 0,
-      'unicorn/no-null': 0,
-      'no-underscore-dangle': 0,
-      'unicorn/no-array-reduce': 0,
-      'unicorn/no-array-callback-reference': 0,
-      'max-len': 0,
-      'import/no-unresolved': 0,
-      'jsdoc/require-returns-description': 0,
-      'jsdoc/require-param-description': 0,
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error',
-      'import/first': 'error',
-      'import/newline-after-import': 'error',
-      'import/no-duplicates': 'error',
+      "prefer-object-spread/prefer-object-spread": 2,
+      "no-restricted-globals": [2, ...restrictedGlobals],
+      "mocha/handle-done-callback": 0,
+      "mocha/no-global-tests": 0,
+      "mocha/valid-test-description": 0,
+      "import/extensions": 0,
+      "import/prefer-default-export": 0,
+      "import/no-cycle": 0,
+      "unicorn/no-null": 0,
+      "no-underscore-dangle": 0,
+      "unicorn/no-array-reduce": 0,
+      "unicorn/no-array-callback-reference": 0,
+      "max-len": 0,
+      "import/no-unresolved": 0,
+      "jsdoc/require-returns-description": 0,
+      "jsdoc/require-param-description": 0,
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      // "import/first": "error",
+      // "import/newline-after-import": "error",
+      // "import/no-duplicates": "error",
     },
   },
 ];
